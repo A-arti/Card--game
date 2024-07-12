4cd1ee7629eff1b3ce0961ca2ebaa1c33f2a6d6b
A-arti <aartikori399@gmail.com>
2024-07-12 18:00:23 +0530

    Some CSS has been applied

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cardObjectDefinitions = [
     {id:1, imagePath:'./images/card-KingHearts.png'},
     {id:2, imagePath:'./images/card-JackClubs.png'},
     {id:3, imagePath:'./images/card-QueenDiamonds.png'},
-    {id:4, imagePath:'./images/AceSpades.png'}
+    {id:4, imagePath:'./images/card-AceSpades.png'}
 ]
 const aceId = 4
 
